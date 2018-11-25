e4c50de0342a0a9252d799e4947d31ea04e9c139
Abner <abner20015@hotmail.com>
2018-11-25 08:02:15 -0800

    CAMBIEN EL INTENT-FILTER AL HACER UPDATE(aqui esta elian)

--- app/src/main/java/com/example/alienware/primerproyecto/LoginActivity.java
+++ app/src/main/java/com/example/alienware/primerproyecto/LoginActivity.java
@@ -10,12 +10,21 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,23 +32,24 @@ public class LoginActivity extends AppCompatActivity {
     Button buttonLogin, buttonRegistrarUsuario;
     Switch switchRecordar;
 
-    HashMap<String,String> user1 = new HashMap<String,String>();
-    HashMap<String,String> user2 = new HashMap<String,String>();
+    String url = "http://www.sicconviene.com/loginAndroid2.php";
+    Map<String, String> params = new HashMap<String,String>();
+    String json = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         loadWidgets();
-        loadUsers();
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isCorrect = checkUser(editTextUsuario.getText().toString(),editTextPassword.getText().toString());
-                if (isCorrect) {
-                    goToActivity(PantallaInicio.class);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Credencial invalida, por favor ingresa nuevamente.",Toast.LENGTH_SHORT).show();
+                loadMap(params);
+                try {
+                    JSONRequest(params);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -47,7 +57,7 @@ public class LoginActivity extends AppCompatActivity {
         buttonRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToActivity(RegistrarUsuarioActivity.class);
+                Utils.goToActivity(getApplicationContext(),RegistrarUsuarioActivity.class);
             }
         });
     }
@@ -58,52 +68,40 @@ public class LoginActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonRegistrarUsuario = findViewById(R.id.buttonRegistrarUsuario);
         switchRecordar = findViewById(R.id.switchRecordarPassword);
-
-
     }
 
-    public String convertHashMap (HashMap hashMap) {
-        Gson gson = new Gson();
-        String hashmapString = gson.toJson(hashMap);
-
+    public void loadMap(Map<String,String> map) {
+        String usuario = editTextUsuario.getText().toString();
+        String password = editTextUsuario.getText().toString();
+        map.put("usuario",usuario);
+        map.put("password",password);
     }
 
-    public void saveOnSharedPreferences (String data) {
-        SharedPreferences preferences = getSharedPreferences("usuario",MODE_PRIVATE);
-        preferences.edit().putString("hashString",data).apply();
-    }
-    public void loadUsers() {
-        user1.put("usuario","aa");
-        user1.put("password","4321");
-        user1.put("nombre","Elian");
-        user1.put("apellido_paterno","Cruz");
-        user1.put("apellido_materno","Esquivel");
-        user1.put("Especialidad","1");
-        user1.put("semestre","5");
-        user1.put("no_control","1");
-        user1.put("edad","17");
-        user1.put("correo","aa@gmail.com");
+    public void JSONRequest(Map<String,String>map) throws JSONException {
+        json = Utils.convertMapToJSONOString(map);
+        JSONObject jsonBody = new JSONObject(json);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (Request.Method.GET, url, jsonBody, new Response.Listener<JSONObject>() {
 
-        user2.put("usuario","lalupis");
-        user2.put("password","1234");
-        user2.put("nombre","Lupita");
-        user2.put("apellido_paterno","Solorio");
-        user2.put("apellido_materno","Duran");
-        user2.put("Especialidad","1");
-        user2.put("semestre","5");
-        user2.put("no_control","555");
-        user2.put("edad","15");
-        user2.put("correo","lupita1112tec@gmail.com");
-    }
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.names().get(0).equals("success")) {
+                        Toast.makeText(getApplicationContext(),response.getString("success"),Toast.LENGTH_SHORT).show();
+                        Utils.saveOnSharedPreferences(getApplicationContext(),json,switchRecordar.isChecked());
+                        Utils.goToActivity(getApplicationContext(),PantallaInicio.class);
+                    } else {
+                        Toast.makeText(getApplicationContext(),response.getString("error"),Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } }
+        }, new Response.ErrorListener() {
 
-    public boolean checkUser(String usuario, String password) {
-        if (usuario.equals(user1.get("usuario"))&&password.equals(user1.get("password"))) { return true; }
-        else if (usuario.equals(user2.get("usuario"))&&password.equals(user2.get("password"))) { return true; }
-        else { return false; }
-    }
-
-    public void goToActivity (Class activity) {
-        Intent intent = new Intent(this,activity);
-        startActivity(intent);
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
